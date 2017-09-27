36109174d14aa9b5ee4f7fb876c869288eedf9b7
Karim Awad <karim.awad17@imperial.ac.uk>
2017-09-27 14:06:12 +0100

    Return variable added, along with titles (#refers to comments or checks)

--- Drafts/data_cleaning_draft2.R
+++ Drafts/data_cleaning_draft2.R
@@ -1,13 +1,18 @@
+##Initial cleaning
 
 old_data <- read.csv("/Drafts/movie_metadata.csv")
-head(old_data)
+#head(old_data)
 filter_data <- filter(old_data, country=="USA", language=="English")
-head(filter_data)
+#head(filter_data)
 select_data <- select(filter_data, movie_title, title_year, director_name, duration, gross, genres, num_voted_users, num_user_for_reviews, budget, imdb_score, movie_facebook_likes, actor_1_name, actor_2_name, actor_3_name)
-head(select_data)
+#head(select_data)
 select_data_filter <- filter(select_data, title_year >= 2011, gross !=0 | gross != "NA" | gross != "", imdb_score != 0 | imdb_score != "NA" | imdb_score != "",budget != 0 | budget != "NA" | budget != "", movie_facebook_likes != 0 | movie_facebook_likes != "NA" | movie_facebook_likes != "")
-head(select_data_filter)
+#head(select_data_filter)
+
 library(tidyr)
+
+##Genre conversion
+
 typeof(select_data_filter$genres)
 toString(select_data_filter$genres)
 str(select_data_filter)
@@ -15,4 +20,10 @@ genre_names <- c("Action","Adventure", "Animation", "Biography", "Comedy", "Crim
 genre_types <- c("genre1", "genre2","genre3", "genre4", "genre5", "genre6", "genre7")
 
 genre_filter <- select_data_filter %>% separate(genres, into=genre_types,sep = "\\|")
-head(genre_filter)
+#head(genre_filter)
+
+##Establishing a Gross Revenue / Budget variable
+
+RB_data <- mutate(.data = genre_filter, return = gross / budget)
+RB_data$return <- round(RB_data$return, 2)
+#head(RB_data)
