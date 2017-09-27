5e8fd2056cad93ea45709a30a49553effa3e4c89
Karim Awad <karim.awad17@imperial.ac.uk>
2017-09-27 12:45:17 +0100

    Initial change

--- Drafts/data_cleaning_draft2.R
+++ Drafts/data_cleaning_draft2.R
@@ -7,3 +7,4 @@ head(select_data)
 select_data_filter <- filter(select_data, title_year >= 2011, gross !=0 | gross != "NA" | gross != "", imdb_score != 0 | imdb_score != "NA" | imdb_score != "",budget != 0 | budget != "NA" | budget != "", movie_facebook_likes != 0 | movie_facebook_likes != "NA" | movie_facebook_likes != "")
 head(select_data_filter)
 library(tidyr)
+trying to send
